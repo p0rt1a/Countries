0bb63c59a1f0f79d105205a5e8323524e4697bed
Alperen Polat <m_alperenPolat@hotmail.com>
2022-07-07 21:00:05 +0300

    api tested and confirmed

---
+++ script.js
@@ -0,0 +1,27 @@
+listCountries();
+
+function listCountries() {
+  const request = new XMLHttpRequest();
+  request.open("GET", "https://restcountries.com/v3.1/all");
+  request.send();
+
+  request.addEventListener("load", () => {
+    const data = JSON.parse(request.responseText);
+    console.log(data);
+
+    for (let i = 0; i < 250; i += 3) {
+      let html = `
+      <div id="card">
+        <img src="${data[i].flags.png}" />
+          <div class="content">
+            <span>${data[i].name.common}</span>
+            <span>Lang: ${data[i].languages[0]}</span>
+            <span>Capital: ${data[i].capital}</span>
+          </div>
+      </div>
+      `;
+
+      document.querySelector("body").insertAdjacentHTML("beforeend", html);
+    }
+  });
+}
